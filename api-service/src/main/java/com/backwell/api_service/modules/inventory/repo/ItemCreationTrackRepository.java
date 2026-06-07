73ecd4e40cd4a2a33d61caed04a435f8037e003e
J-Sinclaire <gonzalez.ortiz.kenneth.xe@gmail.com>
2026-06-06 23:59:31 -0600

    Name Conflict Resolved

--- api-service/src/main/java/com/backwell/api_service/modules/inventory/repo/ItemCreationTrackRepository.java
+++ api-service/src/main/java/com/backwell/api_service/modules/inventory/repo/ItemCreationTrackRepository.java
@@ -1,7 +1,6 @@
 package com.backwell.api_service.modules.inventory.repo;
 
 import com.backwell.api_service.modules.inventory.entity.ItemCreationTrack;
-import com.backwell.api_service.modules.inventory.jooq.repo.ItemCreationTrackCustomRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,7 +9,7 @@ import java.util.UUID;
 @Repository
 public interface ItemCreationTrackRepository extends
         JpaRepository<ItemCreationTrack, UUID>,
-        ItemCreationTrackCustomRepository
+        com.backwell.api_service.modules.inventory.jooq.repo.ItemCreationTrackCustomRepository
 {
 
 }
